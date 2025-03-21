c5f791e527e2aa49b8e9c28e3451144f3550f102
skommana04 <skommana04@gmail.com>
2025-03-21 10:51:10 -0500

    1

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "saibucket0123456789"
+  acl    = "private"
+
+  tags = {
+    Name        = "saibucket0123456789"
+    Environment = "Dev"
+  }
+
+
+}
