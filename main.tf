ac56bbf7d103bf5f0e854dcf4f4d0805fe64e5f3
skommana04 <skommana04@gmail.com>
2025-03-21 11:24:02 -0500

    3

--- main.tf
+++ main.tf
@@ -10,6 +10,13 @@ resource "aws_s3_bucket" "my_bucket" {
     Name        = "saibucket0123456789"
     Environment = "Dev"
   }
-
-
 }
+
+terraform {
+  backend "s3" {
+    bucket = "saibucket0123456789"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
