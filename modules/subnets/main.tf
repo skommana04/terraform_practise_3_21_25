2e95d538036692762f41c9002c199241a003c14d
skommana04 <skommana04@gmail.com>
2025-03-21 16:53:13 -0500

    vpc5-private

--- modules/subnets/main.tf
+++ modules/subnets/main.tf
@@ -25,3 +25,32 @@ resource "aws_subnet" "private_subnet" {
   }
 
 }
+
+resource "aws_igw" "internet_gateway" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name        = "saivpc-igw"
+    Environment = "Dev"
+  }
+}
+
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_igw.internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association" {
+  count          = length(var.public_subnets_cidrs)
+  subnet_id      = element(aws_subnet.public_subnet[*].id, count.index)
+  route_table_id = aws_route_table.public_route_table.id
+
+}
+
+
+
